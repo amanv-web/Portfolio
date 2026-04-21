954a6bfd7d818fc9209b939e575a25f2928212c6
Aman vishwakarma <149085149+amanv-web@users.noreply.github.com>
2026-04-21 08:42:49 +0000

    final

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -74,31 +74,7 @@ function HeroSection() {
         </motion.div>
 
         {/* RIGHT IMAGE */}
-<<<<<<< HEAD
-        <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
-          <div className="relative group">
-            <img
-              src="  <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
-         <div className="w-full aspect-[3/4] overflow-hidden bg-[#020617]">
-  <img
-    src="https://res.cloudinary.com/dsxq0kytm/image/upload/v1776758613/file_00000000ac0c7208a3d9409701245160_bldiug.png"
-    alt="Hero Image"
-    loading="lazy"
-    className="w-full h-full object-cover 
-    transition-transform duration-500 group-hover:scale-105"
-  />
-</div>
-        </div>"
-              alt="Aman Vishwakarma"
-              className="w-52 sm:w-64 md:w-80 rounded-xl object-cover 
-              transition duration-500 
-              group-hover:scale-105 
-              group-hover:shadow-[0_0_30px_rgba(64,214,197,0.4)]"
-              loading="lazy"
-            />
-=======
      <div className="w-full md:w-1/2 flex justify-center relative z-10">
->>>>>>> 112725e (final)
 
   <div className="group">
     
