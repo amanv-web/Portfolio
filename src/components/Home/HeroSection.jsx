aa6c056eb8c004c7d3eedbb7fb23b76d7b723c1e
Aman vishwakarma <149085149+amanv-web@users.noreply.github.com>
2026-04-21 13:44:58 +0530

    img

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -81,7 +81,17 @@ function HeroSection() {
         <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
           <div className="relative group">
             <img
-              src="https://res.cloudinary.com/dsxq0kytm/image/upload/f_auto,q_auto,w_600/v1744608394/Picsart_25-01-29_21-45-03-535_1_k7sue6.jpg"
+              src="  <div className="w-full md:w-1/2 flex justify-center mb-8 md:mb-0">
+         <div className="w-full aspect-[3/4] overflow-hidden bg-[#020617]">
+  <img
+    src="https://res.cloudinary.com/dsxq0kytm/image/upload/v1776758613/file_00000000ac0c7208a3d9409701245160_bldiug.png"
+    alt="Hero Image"
+    loading="lazy"
+    className="w-full h-full object-cover 
+    transition-transform duration-500 group-hover:scale-105"
+  />
+</div>
+        </div>"
               alt="Aman Vishwakarma"
               className="w-52 sm:w-64 md:w-80 rounded-xl object-cover 
               transition duration-500 
